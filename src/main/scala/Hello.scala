503722cc9cbdae818f80a5a094a4114ed74dd9cf
tampler <socnetfpga@gmail.com>
2020-03-01 15:50:08 +0300

    Initial commit

---
+++ src/main/scala/Hello.scala
@@ -0,0 +1,24 @@
+package demo
+
+import cats.implicits._
+
+object Hello extends App {
+
+  val toLong: Int => Long       = _.toLong
+  val toDouble: Float => Double = _.toDouble
+
+  // val f0: ((Int, Float)) => (Long, Double) = Arrow[Function1].split(toLong, toDouble)
+  val f0: ((Int, Float)) => (Long, Double) = toLong *** toDouble
+
+  val res0 = f0((3, 4.0f))
+  println(res0)
+
+  val addEmpty: Int => Int         = _ + 0
+  val multiplyEmpty: Int => Double = _ * 1d
+
+  val f1: Int => (Int, Double) = addEmpty &&& multiplyEmpty
+
+  val res1 = f1(1)
+  println(res1)
+
+}
